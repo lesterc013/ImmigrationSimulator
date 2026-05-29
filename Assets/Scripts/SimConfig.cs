00f6c692762f472365cf6688ce77436c3cbaebfb
mtse-big-best <stetcapdev@gmail.com>
2026-05-30 00:20:46 +0800

    Create Setup menu UI and related elements

---
+++ Assets/Scripts/SimConfig.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace ImmigrationSim
+{
+    [CreateAssetMenu(fileName = "SimConfig", menuName = "Scriptable Objects/SimConfig")]
+    public class SimConfig : ScriptableObject
+    {
+    
+    }
+}
