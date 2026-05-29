ed9064ec545c49913b70e9e62f4c89f726943c70
mtse-big-best <stetcapdev@gmail.com>
2026-05-29 16:25:01 +0800

    Create Server related scripts
    Server is a single instance of a Server to hold a Traveller and release it.
    ServerManager will hold the amount of Servers per user input. It will be the link between QueueController and each of the Servers.
    IServer is the interface that Server will implement so that QueueController will not need to know the exact details of the Server - it just needs to assign the Traveller.

---
+++ Assets/Scripts/Main/Server/IServer.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace ImmigrationSim.Main
+{
+    public class IServer
+    {
+    
+    }
+}
---
+++ Assets/Scripts/Main/Server/ServerManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace ImmigrationSim.Main
+{
+    public class ServerManager : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
