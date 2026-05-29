a501aa44d650ae6f604794cd835b976d63899dd9
mtse-big-best <stetcapdev@gmail.com>
2026-05-29 11:03:46 +0800

    Create SimClock singleton

---
+++ Assets/Scripts/Main/SimClock.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace ImmigrationSim.Main
+{
+    public class SimClock : MonoBehaviour
+    {
+        private static SimClock _instance;
+
+        public static SimClock Instance { get { return _instance; } }
+
+        [SerializeField]
+        [Tooltip("Multiplier for the Sim Clock. Value of 60 would mean 1s irl = 60s SimTime")]
+        private float simSpeed = 60f;
+
+        /// <summary>
+        /// Return the deltaTime multiplied by the simSpeed.
+        /// </summary>
+        public float SimDeltaTime
+        {
+            get { return Time.deltaTime * simSpeed; }
+        }
+
+        public float TotalSimTimeElapsed
+        {
+            get; private set;
+        }
+
+        private void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                _instance = this;
+            }
+        }
+
+        private void Update()
+        {
+            TotalSimTimeElapsed += SimDeltaTime;
+        }
+    }
+}
