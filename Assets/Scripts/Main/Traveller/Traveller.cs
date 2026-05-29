2d59473376018563ee570e3cb35e6b1720bd80bb
mtse-big-best <stetcapdev@gmail.com>
2026-05-29 13:44:48 +0800

    Set basic Traveller fields and methods
    Still not fully ready as need to solve the scalability issue for the QueueJoinTime, ServiceStartTime etc.

--- Assets/Scripts/Main/Traveller/Traveller.cs
+++ Assets/Scripts/Main/Traveller/Traveller.cs
@@ -4,6 +4,40 @@ namespace ImmigrationSim.Main.Traveller
 {
     public class Traveller
     {
-        
+        private static int nextId = 0;
+        public int Id { get; private set; }
+        //needs Id, spawnTime, queueJoinTime, serviceStartTime, TravellerType enum
+        public float SpawnTime { get; private set; }
+        // Is it will need to have 2 different sets of QueueJoin and ServiceStart times? Not very scalable right?
+        // Or should I use a Dictionary to update a Traveller's timings?
+        public float QueueJoinTime { get; private set; }
+        public float ServiceStartTime { get; private set; }
+
+        // TODO: TravellerType Citizen or Foreigner
+
+        public Traveller()
+        {
+            Id = nextId;
+            nextId++;
+            SpawnTime = SimClock.Instance.TotalSimTimeElapsed;
+        }
+
+        public void SetQueueJoinTime(float time)
+        {
+            QueueJoinTime = time;
+        }
+
+        public void SetServiceStartTime(float time)
+        {
+            ServiceStartTime = time;
+        }
+
+        /// <summary>
+        /// To be called when resetting the simulation.
+        /// </summary>
+        public static void ResetTravellerId()
+        {
+            nextId = 0;
+        }
     }
 }
