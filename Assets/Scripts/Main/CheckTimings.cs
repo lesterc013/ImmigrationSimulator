94548f17eb8001ee23527817eb2839c32bad69ed
mtse-big-best <stetcapdev@gmail.com>
2026-05-29 14:08:52 +0800

    Create class to hold a check type's important timings
    Purely a data container to help hold the required timings.

---
+++ Assets/Scripts/Main/CheckTimings.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace ImmigrationSim.Main
+{
+    public class CheckTimings
+    {
+        public float QueueJoinTime { get; set; }
+        public float ServiceStartTime { get; set; }
+        public float ServiceEndTime { get; set; }
+    }
+}
