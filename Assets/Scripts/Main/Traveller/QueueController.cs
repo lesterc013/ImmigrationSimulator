c2b7bde3a61780670c673bae7b3f52bbcb0471f9
mtse-big-best <stetcapdev@gmail.com>
2026-05-29 13:01:54 +0800

    Wire up to receive new Traveller from TravellerSpawner

--- Assets/Scripts/Main/Traveller/QueueController.cs
+++ Assets/Scripts/Main/Traveller/QueueController.cs
@@ -1,9 +1,52 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ImmigrationSim.Main.Traveller
 {
     public class QueueController : MonoBehaviour
     {
-        
+        [SerializeField]
+        [Tooltip("Drag in the correct TravellerEventChannel i.e. Security or Immigration for this case.")]
+        private TravellerEventChannel travellerEc;
+
+        private Queue<Traveller> queue;
+
+        private void Awake()
+        {
+            queue = new Queue<Traveller>();
+
+            travellerEc.OnNewTraveller += HandleNewTraveller;
+            // Need also subscribe to Server Event Channel OnFreeServer to receive a call from a free Server
+        }
+
+        // NewTraveller received
+        // Subscribe to shared event with TravellerSpawner to receive a new Traveller
+        // Push Traveller into Queue
+        // Call TryAssign
+        private void HandleNewTraveller(Traveller newTraveller)
+        {
+            queue.Enqueue(newTraveller);
+            TryAssignTraveller();
+        }
+
+        // TryAssign
+        // check if Queue has something
+        // Check if ServerManager has a free Server
+        // If no, then end
+        // If yes, then get that Server and call the Assign
+        private void TryAssignTraveller()
+        {
+            if (queue.Count == 0)
+            {
+                return;
+            }
+
+            Debug.Log("Check if ServerManager has a free Server, get back a Server interface/object and call the Assign method on it. Then done. Otherwise dont do anything.");
+        }
+
+        private void OnDestroy()
+        {
+            travellerEc.OnNewTraveller -= HandleNewTraveller;
+        }
     }
 }
