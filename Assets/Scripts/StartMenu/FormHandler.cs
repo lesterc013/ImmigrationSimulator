67c196173c30bc4588dfbf328a88e8855fb20dbf
mtse-big-best <stetcapdev@gmail.com>
2026-06-01 21:31:04 +0800

    Change name of Setup to StartMenu

--- Assets/Scripts/StartMenu/FormHandler.cs
+++ Assets/Scripts/StartMenu/FormHandler.cs
@@ -3,7 +3,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace ImmigrationSim.Setup
+namespace ImmigrationSim.StartMenu
 {
     public class FormHandler : MonoBehaviour
     {
