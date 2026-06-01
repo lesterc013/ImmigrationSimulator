93f0217d74963236f4460f309a50e7178d0f28a2
mtse-big-best <stetcapdev@gmail.com>
2026-06-02 00:23:15 +0800

    Implement output params analytics

--- Assets/Scripts/Main/Analytics/AnalyticsEngine.cs
+++ Assets/Scripts/Main/Analytics/AnalyticsEngine.cs
@@ -15,59 +15,120 @@ namespace ImmigrationSim.Main.Analytics
         [SerializeField] private ServerManager securityServerManager;
         [SerializeField] private ServerManager immigrationServerManager;
 
+        // Per stage metrics
         private int securityTravellersTotal;
         private float securityTravellersWaitTotal;
-        private float securityAvgWaitTime;
+        public float SecurityAvgWaitTime { get; private set; }
 
         private int immigrationTravellersTotal;
         private float immigrationTravellersWaitTotal;
-        private float immigrationAvgWaitTime;
+        public float ImmigrationAvgWaitTime { get; private set; }
+
+        // Server utilisation
+        public float SecurityServerUtilisationRate { get; private set; }
+        public float ImmigrationServerUtilisationRate { get; private set; }
+
+        // Global metrics
+        public int TotalTravellersCompleted { get; private set; }
+        public float AvgTotalTimeInSystem { get; private set; }
+        public float TravellersClearedPerMinute { get; private set; }
+        public float PercentageAboveWaitThreshold { get; private set; }
+
+        private float totalTimeInSystem;
+        private int travellersAboveThreshold;
 
         private void Awake()
         {
-            securityQueueEventChannel.OnTravellerLeftQueue += UpdateSecurityAvgWaitTimeDisplay;
-            immigrationQueueEventChannel.OnTravellerLeftQueue += UpdateImmigrationAvgWaitTimeDisplay;
+            securityQueueEventChannel.OnTravellerLeftQueue += UpdateSecurityAvgWaitTime;
+            immigrationQueueEventChannel.OnTravellerLeftQueue += UpdateImmigrationAvgWaitTime;
+            finalStageServerEventChannel.OnTravellerExitingService += HandleTravellerCompletedData;
         }
 
-        private void UpdateSecurityAvgWaitTimeDisplay(TravellerEntity securityTraveller)
+        private void Update()
         {
-            UpdateStageTypeData(StageType.Security, securityTraveller);
-            // TODO: Update the TMP Text display
-            Debug.Log($"Security avg wait time: {securityAvgWaitTime}");
+            if (SimClock.Instance.IsFinished) 
+                return;
+
+            SecurityServerUtilisationRate = CalculateServerUtilisationRate(securityServerManager.GetTotalServersBusyTime());
+            ImmigrationServerUtilisationRate = CalculateServerUtilisationRate(immigrationServerManager.GetTotalServersBusyTime());
+            if (TotalTravellersCompleted > 0)
+            {
+                TravellersClearedPerMinute = TotalTravellersCompleted / (SimClock.Instance.TotalSimTimeElapsed / 60f);
+            }
         }
 
-        private void UpdateImmigrationAvgWaitTimeDisplay(TravellerEntity securityTraveller)
+        private void UpdateSecurityAvgWaitTime(TravellerEntity securityTraveller)
         {
-            UpdateStageTypeData(StageType.Immigration, securityTraveller);
+            UpdateStageWaitTime(StageType.Security, securityTraveller);
             // TODO: Update the TMP Text display
-            Debug.Log($"Immigration avg wait time: {immigrationAvgWaitTime}");
         }
 
-        private void UpdateStageTypeData(StageType stage, TravellerEntity traveller)
+        private void UpdateImmigrationAvgWaitTime(TravellerEntity securityTraveller)
+        {
+            UpdateStageWaitTime(StageType.Immigration, securityTraveller);
+            // TODO: Update the TMP Text display
+        }
+
+        private void UpdateStageWaitTime(StageType stage, TravellerEntity traveller)
         {
             float waitTime = traveller.Timings[stage].ServiceStartTime - traveller.Timings[stage].QueueJoinTime;
-
             if (stage == StageType.Security)
             {
-                securityTravellersTotal += 1;
+                securityTravellersTotal++;
                 securityTravellersWaitTotal += waitTime;
-                securityAvgWaitTime = securityTravellersWaitTotal / securityTravellersTotal;
+                SecurityAvgWaitTime = securityTravellersWaitTotal / securityTravellersTotal;
+                Debug.Log($"[Analytics] Security traveller {traveller.Id} wait: {waitTime:F2}s | Running avg: {SecurityAvgWaitTime:F2}s | Count: {securityTravellersTotal}");
             }
             else if (stage == StageType.Immigration)
             {
-                immigrationTravellersTotal += 1;
+                immigrationTravellersTotal++;
                 immigrationTravellersWaitTotal += waitTime;
-                immigrationAvgWaitTime = immigrationTravellersWaitTotal / immigrationTravellersTotal;
+                ImmigrationAvgWaitTime = immigrationTravellersWaitTotal / immigrationTravellersTotal;
+                Debug.Log($"[Analytics] Immigration traveller {traveller.Id} wait: {waitTime:F2}s | Running avg: {ImmigrationAvgWaitTime:F2}s | Count: {immigrationTravellersTotal}");
             }
             else
             {
-                Debug.LogError($"{stage} not set as a selector.");
+                Debug.LogError($"AnalyticsEngine: unhandled StageType {stage}");
             }
+        }
+
+        private float CalculateServerUtilisationRate(float serverTotalBusyTime)
+        {
+            if (SimClock.Instance.TotalSimTimeElapsed <= 0)
+                return 0;
+
+            return (serverTotalBusyTime / SimClock.Instance.TotalSimTimeElapsed) * 100;
+        }
+
+        private void HandleTravellerCompletedData(TravellerEntity traveller)
+        {
+            TotalTravellersCompleted++;
+
+            // Avg total time in system
+            float timeInSystem = SimClock.Instance.TotalSimTimeElapsed - traveller.SpawnTime;
+            totalTimeInSystem += timeInSystem;
+            AvgTotalTimeInSystem = totalTimeInSystem / TotalTravellersCompleted;
+
+            // % exceeding threshold
+            float totalWaitTime = 0;
+            foreach (var timings in traveller.Timings.Values)
+            {
+                totalWaitTime += timings.ServiceStartTime - timings.QueueJoinTime;
+            }
+            if (totalWaitTime > simConfig.WaitThreshold)
+            {
+                travellersAboveThreshold++;
+            }
+            PercentageAboveWaitThreshold = (float)travellersAboveThreshold / TotalTravellersCompleted * 100f;
+
+            Debug.Log($"[Analytics] Traveller {traveller.Id} completed | TimeInSystem: {timeInSystem:F2}s | TotalWaitTime: {totalWaitTime:F2}s | AboveThreshold: {totalWaitTime > simConfig.WaitThreshold} | % Above: {PercentageAboveWaitThreshold:F1}% | Throughput: {TravellersClearedPerMinute:F2}/min");
         }
 
         private void OnDestroy()
         {
-            securityQueueEventChannel.OnTravellerLeftQueue -= UpdateSecurityAvgWaitTimeDisplay;
+            securityQueueEventChannel.OnTravellerLeftQueue -= UpdateSecurityAvgWaitTime;
+            immigrationQueueEventChannel.OnTravellerLeftQueue -= UpdateImmigrationAvgWaitTime;
+            finalStageServerEventChannel.OnTravellerExitingService -= HandleTravellerCompletedData;
         }
     }
 }
