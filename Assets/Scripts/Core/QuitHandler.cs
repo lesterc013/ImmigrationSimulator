6e6f96e5877f0399bd913a4c5fae031fc40cf175
mtse-big-best <stetcapdev@gmail.com>
2026-06-02 15:51:25 +0800

    Add Quit button

---
+++ Assets/Scripts/Core/QuitHandler.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace ImmigrationSim.Core
+{
+    public class QuitHandler : MonoBehaviour
+    {
+        public void QuitSim()
+        {
+            Application.Quit();
+        }
+    }
+}
